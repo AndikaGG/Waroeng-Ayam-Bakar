573f8a28c624aa89e0d992b82c217074854026ac
AndikaGG <ya166260@gmail.com>
2023-11-30 13:38:21 +0700

    menambahkan acara pada beberapa elemen

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ window.addEventListener("load", function () {
   preloader.classList.add("loaded");
   document.body.classList.add("loaded");
 });
+
+/**
+ * tambahkan pendengar acara pada beberapa elemen
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+      elements[i].addEventListener(eventType, callback);
+    }
+  }
